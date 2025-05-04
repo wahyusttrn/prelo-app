94ec2b9b0efdac2dfa9a4d97672f55652044a747
WahyuSattriana <wahyusattriana@gmail.com>
2025-05-05 00:18:49 +0800

    feat: fix link and add explanation

--- login/script.js
+++ login/script.js
@@ -2,7 +2,7 @@ function login() {
   const username = document.getElementById('username');
   if (username.value === 'budi') {
     alert('kamu berhasil login!');
-    location.href = '../index.html';
+    location.href = '../'; //this is how we get to parent directory
   } else {
     alert('maaf, kamu bukan budi');
   }
