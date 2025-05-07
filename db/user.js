5a229984ed0940cbcb7ac3fd0705b726072e8d58
WahyuSattriana <wahyusattriana@gmail.com>
2025-05-07 18:43:06 +0800

    feat: update directory system and fix: profile page bug

---
+++ db/user.js
@@ -0,0 +1,25 @@
+const user = [
+  {
+    id: 1,
+    email: 'budi@hotmail.com',
+    username: 'budianjay',
+    password: '!Budi123',
+    namaLengkap: 'Budi Doremi'
+  },
+  {
+    id: 2,
+    email: 'andi@yahoo.com',
+    username: 'andijos',
+    password: '#Andi1212',
+    namaLengkap: 'Andi Kick'
+  },
+  {
+    id: 3,
+    email: 'susanti@wow.net',
+    username: 'susanticuy',
+    password: 'Susanti&20',
+    namaLengkap: 'Susanti Cuy'
+  }
+];
+
+export default user;
