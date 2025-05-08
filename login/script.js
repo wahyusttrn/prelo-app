692bdc41da208cda256e1c637d3aa5c199cb2520
Rama <syahrul.rama33@gmail.com>
2025-05-08 19:10:22 +0700

    update login dan pembuata folder signup

--- login/script.js
+++ login/script.js
@@ -2,13 +2,12 @@ import USERS from '../db/USERS.js';
 
 const form = document.getElementById('loginForm');
 form.addEventListener('submit', login);
+
 function login(loggedIn) {
   loggedIn.preventDefault();
   const usernameInput = document.getElementById('username').value;
   const passwordInput = document.getElementById('password').value;
-
   let nameUser = "";
-
   for (let i = 0; i < USERS.length; i++) {
     const { username, password, namaLengkap } = USERS[i];
     if (usernameInput === username && passwordInput === password) {
@@ -19,7 +18,6 @@ function login(loggedIn) {
       break;
     }
   }
-
   if (JSON.parse(localStorage.getItem('loginStatus')) === true) {
     alert('Kamu berhasil login!');
     greeting(nameUser);
@@ -29,15 +27,25 @@ function login(loggedIn) {
   }
 }
 
+function signUp() {
+  location.href = '../sign up/index.html'   // sign up\index.html
+}
+
+function goBack() {
+  location.href = '../';      //this is how we get to parent directory
+}
+
+window.login   = login;
+window.signUp  = signUp;
+window.goBack  = goBack;
+window.logout  = logout;
+
+
 function logout() {
   localStorage.removeItem("loginStatus");
   localStorage.removeItem("username");
   localStorage.removeItem("fullName");
   location.reload();      // refresh page
-}
-
-function goBack() {
-  location.href = '../';      //this is how we get to parent directory
 }
 
 // function greeting(nameUser) {
@@ -46,7 +54,3 @@ function goBack() {
 //     greeting.innerText = `Selamat datang, ${nameUser}!`;
 //   }
 // }
-
-window.login   = login;
-window.logout  = logout;
-window.goBack  = goBack;
